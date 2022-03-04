29b085d91a1fabce2883645fe7630de407fdc06c
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-03-04 16:27:02 +1100

    Added entrypoint to program

---
+++ book_tracker.py
@@ -0,0 +1,11 @@
+import sys
+from main_window import MainWindow
+from PyQt6.QtWidgets import QApplication
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    # window.loadStyle("styles/main-window.css")
+    window.show()
+    sys.exit(app.exec())
