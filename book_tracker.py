f4d386bf262eacff6487bdab59d03e6c14276be8
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-03-23 12:11:33 +1100

    Apply styling to program

--- book_tracker.py
+++ book_tracker.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     window = MainWindow()
-    # window.loadStyle("styles/main-window.css")
+    window.loadStyle("styles/main-window.css")
     window.show()
     sys.exit(app.exec())
